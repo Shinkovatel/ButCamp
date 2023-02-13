d11150154f436e34d2d9686eb80f705862014919
Sergey Bushelenkov <bsn1980@yandex.ru>
2023-02-13 18:52:57 +0300

    дописано 2 программы

--- Lession1/Program.cs
+++ Lession1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* - открытие многострочного комментация 
+
+// тип данных имя переменной = значение
+Console.Write ("Введите число для которого мы ищем квадрат: ");
+int numberA = Convert.ToInt32(Console.ReadLine()); 
+// Математика
+int result = numberA * numberA;
+// rezult
+Console.WriteLine("Квадрат числа равен = " + numberA + " равен " + result);
+*/
+Console.Write("Введите 1-ое число:");
+int firstValue = Convert.ToInt32(Console.ReadLine()); //camelcase 
+Console.Write("Введите 2-ое число:");
+int secondValue = Convert.ToInt32(Console.ReadLine());
+// Точка с запятой не ставиться перед фигурными скобками "{"
+if (firstValue == Math.Pow(secondValue,2)) // math.pow(secondValue,2) или secondValue*secondValue
+{
+    Console.WriteLine("Число " + firstValue + "является квадратом от" + secondValue);
+}
+else // что не является квадратом
+
+{
+Console.WriteLine(" Число " + firstValue + " не является квадратом от " + secondValue);
+}
+int result = firstValue + secondValue; // результат   
+Console.WriteLine("", result);
