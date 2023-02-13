ea05c44764aff053cbac1780926dd9b0a1d36d7a
Sergey Bushelenkov <bsn1980@yandex.ru>
2023-02-13 19:27:49 +0300

    добавили папку с 1 ДЗ

---
+++ Homework1/task1/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите первое число:");
+int firstValue = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число:");
+int secondValue = Convert.ToInt32(Console.ReadLine());
+
+ int rezult = 0;
+
+if (firstValue == secondValue)
+{
+    Console.WriteLine(" Первое число " + firstValue + " равно второму числу " + secondValue);
+}
+else
+{
+    if (firstValue > secondValue)
+    { 
+    rezult = firstValue - secondValue;
+    Console.WriteLine(" Первое число " + firstValue + " больше второго числа " + secondValue + " на " + rezult);
+    }
+    else
+    {
+    rezult = secondValue - firstValue;
+    Console.WriteLine(" Второе Число " + secondValue + " больше первого числа " + firstValue + " на " + rezult);  
+    }
+}
