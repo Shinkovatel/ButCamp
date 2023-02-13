9177e0c6a191d984de38b275ddd7c99c85e1d826
Sergey Bushelenkov <bsn1980@yandex.ru>
2023-02-13 21:38:06 +0300

    добавили папку с 3-м заданием

--- Homework1/task2/Program.cs
+++ Homework1/task2/Program.cs
@@ -7,7 +7,7 @@ int secondValue = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите третье число:");  // вводим третье число
 int thidValue = Convert.ToInt32(Console.ReadLine());
 
-int maxValue = 0; // задаем начальное значение для сравнения
+int maxValue = 0 ; // задаем начальное значение для сравнения
  
 if (firstValue > maxValue) // проверка 1-го числа
 {
@@ -24,3 +24,4 @@ if (thidValue > maxValue) // проверка 3-го числа
 
 Console.WriteLine("Максимальное число равно  = " + maxValue); // вывод
 
+// не работает для отрицательных значений
---
+++ Homework1/task3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число:"); // вводим первое число
+int firstValue = Convert.ToInt32(Console.ReadLine()); // вносим в переменную
+
+
+if (firstValue % 2 != 0) // проверка если число не делиться на 2 без остатка
+{
+    Console.WriteLine(" число " + firstValue + " нечетное ");
+}
+else
+{
+    Console.WriteLine(" число " + firstValue + " четное ");
+}
