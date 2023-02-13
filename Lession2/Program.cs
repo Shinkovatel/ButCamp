f68ed037dc153e6bb0eab5c8374400fc91b35d70
Sergey Bushelenkov <bsn1980@yandex.ru>
2023-02-13 22:33:15 +0300

    решити задачу с методом

--- Lession2/Program.cs
+++ Lession2/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3; 
+    return result;
+}
+
+
+int a1 = 3;
+int a2 = 4;
+int a3 = 6;
+int b1 = 7;
+int b2 = 8;
+int b3 = 18;
+int c1 = 521;
+int c2 = 53;
+int c3 = 12;
+
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+
+// int max = Max(max1, max2, max3);
+
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3));
+ 
+Console.WriteLine(max);
