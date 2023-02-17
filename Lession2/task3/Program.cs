73c105162c12a995140c78b8a3a63ba2ed6c9e97
[Shinko] <bsn1980@yandex.ru>
2023-02-17 09:22:59 +0300

    дописали 3-е задание

--- Lession2/task3/Program.cs
+++ Lession2/task3/Program.cs
@@ -1,5 +1,53 @@
-﻿dffnlkasnd;sealeds
-f'd
-asfjsa
-namespace d
-sad;
+﻿void fullarray (int[] collection)
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        collection[index] = new Random().Next(1,10);
+      //Index = index + 1;
+        index++;
+    }    
+}
+
+void PrintArray (int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++; 
+    }
+}
+
+int indexOf (int[]  collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index = index + 1;
+
+    }
+    return position;
+
+}
+
+int[] array =   new int[10];
+
+fullarray(array);
+array[4] = 4;
+array[6] = 4; 
+PrintArray(array);
+Console.WriteLine();
+
+int pos = indexOf (array, 444);
+Console.WriteLine(pos);
