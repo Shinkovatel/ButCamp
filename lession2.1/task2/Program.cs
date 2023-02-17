ba836d8d475eb06a215a058139652e69ead38f2e
[Shinko] <bsn1980@yandex.ru>
2023-02-17 10:25:45 +0300

    ...

---
+++ lession2.1/task2/Program.cs
@@ -0,0 +1,6 @@
+﻿//int Number = new Random().Next(10, 100); // по задаче нужно от 10, до 99, правая граниа на 1 больше
+
+//int Number = new Random().Next(10); // первый параметр равен 0
+
+int Number = new Random().NextDouble() // возвращает от 0 до 1
+
