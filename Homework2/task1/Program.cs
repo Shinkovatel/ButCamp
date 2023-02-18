37fa03079ed30cd8275ebac1a6dd6801452a2cf1
Sergey Bushelenkov <bsn1980@yandex.ru>
2023-02-18 21:41:10 +0300

    Сделали ДЗ на ввод трехзначного числа

--- Homework2/task1/Program.cs
+++ Homework2/task1/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿Console.Write("Введите трехзначное число:"); // вводим первое число
+int Value = Convert.ToInt32(Console.ReadLine()); // вносим в переменную
+
+if ((Value/100) < 1 || ((Value/100) > 10))
+{
+    Console.WriteLine("введенное число не является трехзначным");   
+    return;
+}
+
+int result = (Value / 10) - ((Value / 100)*10);
+Console.WriteLine (result); 
+
