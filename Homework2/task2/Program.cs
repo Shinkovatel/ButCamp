d27a97034ac47460ed08ae35a2f2005b3f2473e6
Sergey Bushelenkov <bsn1980@yandex.ru>
2023-02-18 22:13:25 +0300

    Дополнил 2-е ДЗ с семизначным числом

--- Homework2/task2/Program.cs
+++ Homework2/task2/Program.cs
@@ -6,5 +6,17 @@ if ((Value/100) < 1 ) // Проверка что число трехзначно
     Console.WriteLine("Нет третьей цифры");   
     return;
 }
-int result = (Value / 10000) - ((Value / 100000)*10);
-Console.WriteLine (result); 
+
+Console.Write("Ищем третью цифру с начала (нажмите 1) или с конца (нажмите 2) :"); // определяем условие выбора
+int Valuevariant = Convert.ToInt32(Console.ReadLine()); // вносим в переменную
+
+ if (Valuevariant == 1)
+ {
+    int result = (Value / 10000) - ((Value / 100000)*10);
+    Console.WriteLine (result); 
+ }
+ else
+ {
+    int result = (Value / 100) - ((Value / 1000)*10);
+    Console.WriteLine (result); 
+ }
