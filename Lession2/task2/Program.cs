83947b68bdcd769442b218080ddd3968fcd1290b
Sergey Bushelenkov <bsn1980@yandex.ru>
2023-02-18 22:14:38 +0300

    обновил  данные

--- Lession2/task2/Program.cs
+++ Lession2/task2/Program.cs
@@ -1,14 +1,16 @@
-﻿int[] array = {1,2,3,4,5,6,7,8,9,10};
+﻿int[] array = { 1,2,3,4,5,6,7,8,9,10 };
 
 int n = array.Length;
-int find = 4;
+int find = 9;
+
 int index = 0;
 
 while (index < n)
 {
     if (array[index] == find)
     {
-        Console.WriteLine(index);   
+        Console.WriteLine(index); 
+        break;  
     }
-    index =+1;
+    index++;
 } 
