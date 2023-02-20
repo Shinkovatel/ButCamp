04b50128ad4ce2e996f93106499038768c319dc8
Sergey Bushelenkov <bsn1980@yandex.ru>
2023-02-20 20:43:59 +0300

    добавил 3-ю Семинар

---
+++ Lession3S/Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿
+// проверка на значность
+
+// int number = 100;
+
+// if (number >= 100 && number <= 999) 
+// {
+//     Console.WriteLine(" Число трехзначное")
+// }
+// else
+// {
+//     Console.WriteLine(" Число не трехзначное")
+// }
+
+// string testNumber = Console.ReadLine();
+
+// Console.WriteLine(testNumber[0]); // " 9 "
+
+// Console.WriteLine(testNumber[1]); // " 0 "
+
+// Console.WriteLine(testNumber[2]); // " 9 "  
+
+// var имя переменной = значение
+
+var stringNumber = "1234"
+
+int number; // если строчка состоит из чисел, то сюда попадает наше число
+
+
+if ()
+
+
+Console.WriteLine(number);
