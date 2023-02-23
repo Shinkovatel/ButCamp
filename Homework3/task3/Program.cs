43b4d9cd58d29913f474fb8de6faafc951a1058b
Sergey Bushelenkov <bsn1980@yandex.ru>
2023-02-23 18:17:15 +0300

    Добавлена задача № 3 в Homework3

--- Homework3/task3/Program.cs
+++ Homework3/task3/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿input1:
+Console.Write("Введите число N: ");
+bool check_N = int.TryParse(Console.ReadLine(), out int N);
+if (!check_N)
+{
+    Console.WriteLine("Введены не верные данные");
+    goto input1;
+}
+
+for(int i = 1; i <= N; i+=1)
+{
+    Console.Write(Math.Pow(i,3) + "\t");
+} 
