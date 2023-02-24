69ee321ac768c6cd3802b31dbb991ea3a302fe1b
Sergey Bushelenkov <bsn1980@yandex.ru>
2023-02-24 21:30:59 +0300

    Добавили 2 задание к Lessoin5s

---
+++ Lession5s/task2/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] Array = GetArray(5, -100, 100);
+int size = Array.Length;
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result; //вернули массив на Size элементов, заполненный числами
+}
+int Number = Convert.ToInt32(Console.ReadLine());
+bool IsMatch = false;
+for (int i = 0; i < size; i++)
+{
+    if (Number == Array[i])
+    {
+        IsMatch = true;
+        break;
+    }
+
+}
+
+if (IsMatch)
+{
+    Console.WriteLine("Число найдено");
+}
+else
+{
+    Console.WriteLine("Число ненайдено");
+}
+
+Console.WriteLine($"Исходный массив: [{String.Join("; ", Array)}]");
