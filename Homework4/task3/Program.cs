594c66ff1a3e81b6839753688ed62c0e1484f44a
Sergey Bushelenkov <bsn1980@yandex.ru>
2023-02-24 12:41:21 +0300

    Добавили 3 задание к Homowork4

---
+++ Homework4/task3/Program.cs
@@ -0,0 +1,18 @@
+﻿input1:
+Console.Write("Введите число : ");
+bool check_Number = int.TryParse(Console.ReadLine(), out int Number);
+if (!check_Number)
+{
+    Console.WriteLine("Введены не верные данные");
+    goto input1;
+}
+ string SecondNumber = Number.ToString();
+ int[] resultArray = new int[SecondNumber.Length];
+
+for (int i = 0; i < resultArray.Length; i++)
+{
+    resultArray[i] = int.Parse(SecondNumber[i].ToString());
+
+}
+
+ Console.WriteLine($"Массив [{String.Join("; ", resultArray)}]");
