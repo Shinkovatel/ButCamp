018d7926b1abe05536eb62349f991ecdbd91e6ac
Sergey Bushelenkov <bsn1980@yandex.ru>
2023-02-24 11:48:27 +0300

    Добавили 1 задание к Homowork4

---
+++ Homework4/task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введете число А ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введете степень в которую нужно возвести число  ");
+int degree = Convert.ToInt32(Console.ReadLine());
+
+int sum = 1;
+for(int i = 0; i <= degree-1; i++) 
+{
+    sum = sum*Number;
+    
+}
+
+Console.WriteLine(sum);
