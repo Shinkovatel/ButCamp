dc563121094f88b54d79b8727ad6ed9ef0718642
Sergey Bushelenkov <bsn1980@yandex.ru>
2023-02-24 12:28:11 +0300

    Добавили 2 задание к Homowork4

--- Homework4/task1/Program.cs
+++ Homework4/task1/Program.cs
@@ -1,9 +1,19 @@
-﻿Console.Write("Введете число А ");
-int Number = Convert.ToInt32(Console.ReadLine());
-
+﻿input1:
+Console.Write("Введете число А ");
+bool check_Number = int.TryParse(Console.ReadLine(), out int Number);
+if (!check_Number)
+{
+    Console.WriteLine("Введены не верные данные");
+    goto input1;
+}
+input2:
 Console.Write("Введете степень в которую нужно возвести число  ");
-int degree = Convert.ToInt32(Console.ReadLine());
-
+bool check_degree = int.TryParse(Console.ReadLine(), out int degree);
+if (!check_degree)
+{
+    Console.WriteLine("Введены не верные данные");
+    goto input2;
+}
 int sum = 1;
 for(int i = 0; i <= degree-1; i++) 
 {
---
+++ Homework4/task2/Program.cs
@@ -0,0 +1,21 @@
+﻿
+input1:
+Console.Write("Введите число : ");
+bool check_Number = int.TryParse(Console.ReadLine(), out int Number);
+if (!check_Number)
+{
+    Console.WriteLine("Введены не верные данные");
+    goto input1;
+}
+int sum=0;
+
+while (Number != 0) 
+{
+    sum+= Number % 10;
+    Number = Number / 10;
+}
+
+Console.WriteLine($"Сумма чисел равна {sum}");
+
+// int result = Number.ToString().Sum(c=> c- '0');
+// Console.WriteLine($"Сумма чисел равна {result}");
