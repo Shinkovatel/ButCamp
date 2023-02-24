258c8af2ffd37937628c1cb49fc80a3a1cd1849d
Sergey Bushelenkov <bsn1980@yandex.ru>
2023-02-24 10:00:18 +0300

    Добавили 4-ю Лекцию, 1 задание

---
+++ Lession4S/task1/Program.cs
@@ -0,0 +1,61 @@
+﻿// // void : нет оператора return
+
+// // тип возращаемого значения или (параметры)
+
+//  int Calculate(int a, int b, char sign) // Два числа: a,b и знак(* \ )
+//  {
+//     //int result = a+b;
+//     int result = 0;
+//     if (sign == '*')
+//     {
+//         result = a * b ;
+//     }
+//     else if (sign == '+')
+//     {
+//         result = a + b;
+//     }
+//     else if (sign == '-')
+//     {
+//         result = a - b;
+//     }
+//     // else  (sign == '/')
+//     // { 
+//     //     result = a / b; 
+//     // }
+//     return result;
+//  }
+
+
+// Console.WriteLine(Calculate(3,5, '+'));
+// Console.WriteLine(Calculate(3,5, '-'));
+// Console.WriteLine(Calculate(3,5, '*'));
+// Console.WriteLine(Calculate(3,5, '_'));
+// // string - массив символов
+// // char - один символ
+
+Console.Write("Введете число  ");
+int limit = Convert.ToInt32(Console.ReadLine());
+
+// int sum = 0;
+
+// for (int i = 0; i <= limit; i++)
+// {
+//     sum += i; // a = a+5 = (a+=5) ; a = a*200 = (a*=200); f=f/400 = (f/=400)
+// }
+ 
+// Console.WriteLine($"Сумма числа от 1 до {limit} = {sum}");
+// pascalcase - каждое слово с большой буквы
+int GetSum(int A)
+{
+    A+=100;
+    int sum = 0;
+
+    for (int i = 0; i <= A; i++)
+    {
+    sum += i; 
+    }
+    return sum;
+}
+
+Console.WriteLine($"Сумма числа от 1 до {limit} = {GetSum(limit)}");
+Console.WriteLine($"Сумма числа от 1 до {4} = {GetSum(4)}");
