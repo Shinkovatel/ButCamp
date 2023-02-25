d67af0acc3ae1a392f63ee80f6b1253765d1a0e5
Sergey Bushelenkov <bsn1980@yandex.ru>
2023-02-25 23:01:50 +0300

    Добавили 3 задание  homework5

---
+++ Homework5/task3/Program.cs
@@ -0,0 +1,48 @@
+﻿Double[] RandomArray(int size)
+{
+    Double[] result = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().NextDouble();
+    }
+    return result; //вернули массив на Size элементов, заполненный числами
+}
+
+Double[] realArray = RandomArray(5);
+int size = realArray.Length;
+
+var t = 100;
+
+for (int i = 0; i < size; i++) // умножили каждый элемент массива на 100
+{
+    realArray[i] = t * realArray[i];
+}
+Console.WriteLine($"[{String.Join(";", realArray)}]"); // для проверки вывода
+
+// преобразовали массив из Double в int, чтобы удобнее было считать
+    int[] PsevdoArray = new int[realArray.Length];
+    for (int i = 0; i < realArray.Length; i++)
+    {
+        PsevdoArray[i] = (int)Math.Ceiling(realArray[i]);
+    }
+
+Console.WriteLine($"[{String.Join(";", PsevdoArray)}]"); // для проверки вывода
+
+//  поиск максимального и минимального элемента массива
+int size2 = PsevdoArray.Length;
+int minValue = PsevdoArray[0];
+int maxValue = PsevdoArray[0];
+for (int i = 0; i < size2; i++)
+{
+    if (maxValue < PsevdoArray[i])
+    {
+        maxValue = PsevdoArray[i];
+    }
+    else if (minValue > PsevdoArray[i])
+    {
+        minValue = PsevdoArray[i];
+    }
+
+}
+
+Console.WriteLine($"Минимальное значение массива {minValue}" + "\n" + $"Максимальное значение массива {maxValue}");
