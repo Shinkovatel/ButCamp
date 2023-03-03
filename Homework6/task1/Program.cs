fb0877d18c8ae745a26d53bcd082bef59f801f5d
Sergey Bushelenkov <bsn1980@yandex.ru>
2023-03-03 23:41:40 +0300

    Внес комментарии в 1-ю задачу ДЗ 6

--- Homework6/task1/Program.cs
+++ Homework6/task1/Program.cs
@@ -4,13 +4,13 @@ string[] values = valueStr.Split(","); // числа по отдельности
 
 float[] PointA= new float[values.Length]; // определили новый массив, с длиной массива values
 int j = 0;
-foreach (string value in values)
+foreach (string value in values) // перебираем элементы массива и записываем с преобразованием
     PointA[j++] = Convert.ToSingle(value);
  
 int razmer=PointA.Length;
 
 int count = 0;
-for (int i = 0; i < razmer; i++)
+for (int i = 0; i < razmer; i++) // проверяем элементы массива на положительные числа
 {
     if (PointA[i]> 0)
     {
