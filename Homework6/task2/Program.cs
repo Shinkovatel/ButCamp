d69e2415c4e49497fd4907685eb4e95d4b5fa50c
Sergey Bushelenkov <bsn1980@yandex.ru>
2023-03-05 11:25:20 +0300

    Внес изменения в 2-ю и  3-ю задачу ДЗ 6

---
+++ Homework6/task2/Program.cs
@@ -0,0 +1,40 @@
+﻿input1:
+Console.Write("Введите K1 : ");
+bool check_koff1 = int.TryParse(Console.ReadLine(), out int koff1);
+if (!check_koff1)
+{
+    Console.WriteLine("Введены не верные данные");
+    goto input1;
+}
+
+input1:
+Console.Write("Введите число K2: ");
+bool check_koff2 = int.TryParse(Console.ReadLine(), out int koff2);
+if (!check_koff2)
+{
+    Console.WriteLine("Введены не верные данные");
+    goto input1;
+}
+
+input1:
+Console.Write("Введите число B1: ");
+bool check_B1 = int.TryParse(Console.ReadLine(), out int B1);
+if (!check_B1)
+{
+    Console.WriteLine("Введены не верные данные");
+    goto input1;
+}
+input1:
+Console.Write("Введите число B2 : ");
+bool check_B2 = int.TryParse(Console.ReadLine(), out int B2);
+if (!check_B2)
+{
+    Console.WriteLine("Введены не верные данные");
+    goto input1;
+}
+
+int equation_x = koff1 * x;
+
+int equation_y = 0;
+
+
