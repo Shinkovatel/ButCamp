f120832fc89a6417c3ad812b6125c7bda7261785
Sergey Bushelenkov <bsn1980@yandex.ru>
2023-03-08 14:18:44 +0300

    Добавили 1 задание в Homework7

---
+++ Homework7/task1/Program.cs
@@ -0,0 +1,58 @@
+﻿double[,] GetArray(int rows, int cols)
+{
+
+    double[,] array = new double[rows, cols];
+    for (int row = 0; row < rows; row++)
+    {
+        for (int col = 0; col < cols; col++)
+        {
+            array[row, col] = new Random().NextDouble();
+        }
+    }
+    return array;
+}
+
+int[,] Transform(double[,] array)
+{
+var t = 100; 
+    int[,] PseudoArray = new int[array.GetLength(0),array.GetLength(1)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            PseudoArray[i, j] = (int)Math.Ceiling(array[i, j] * t);
+        }
+    }
+    return PseudoArray;
+}
+
+void PrintMatrixDouble(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr[i, j] + "\t ");
+
+        }
+        Console.WriteLine();
+    }
+}
+
+void PrintMatrixint(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr[i, j] + "\t ");
+
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] result = GetArray(3,4);
+PrintMatrixDouble(result);
+int[,] FinalArray = Transform(result);
+PrintMatrixint(FinalArray);
