f395ccae6f6fdeaebfb43071913be9cb3dd87e2a
Sergey Bushelenkov <bsn1980@yandex.ru>
2023-03-12 12:59:31 +0300

    Добавили 1 задание в ДЗ № 9

--- Lession9s/task1/Program.cs
+++ Lession9s/task1/Program.cs
@@ -1,75 +1,75 @@
-﻿// // унарные операторы - 1 операнд
+﻿// унарные операторы - 1 операнд
 
-// internal a = 5; 
+internal a = 5; 
 
 
 
-// // бинарные операторы - 2 операнда
+// бинарные операторы - 2 операнда
 
-// int a = 5 + 2;
-// int b = 5 % 3;
+int a = 5 + 2;
+int b = 5 % 3;
 
-// // посфикс: "после"
+// посфикс: "после"
 
-// a++; 
+a++; 
 
-// // Префикс
+// Префикс
 
-// ++ a; -  // у префиксного оператора наибольший приоритет
+++ a; -  // у префиксного оператора наибольший приоритет
 
 
 // Тернарный оператор, оперирует с 3-мя операндами
 
 // условие 1 - операнд, 2 - значение для истины, 3 - значение для лжи
 
-// int dreamSalary = int.MaxValue;
-// int realSalary = 20000;
-// var result = realSalary < dreamSalary ? "Пока что не такая большая" : "Наша зарплата огромна"; // для простого действия
+int dreamSalary = int.MaxValue;
+int realSalary = 20000;
+var result = realSalary < dreamSalary ? "Пока что не такая большая" : "Наша зарплата огромна"; // для простого действия
 
-// Console.WriteLine(result);
+Console.WriteLine(result);
 
-// Рекурсия
+Рекурсия
 
-// string SayHello()
-// {
-//     SayHello();
-//     return "hi";
-// }
+string SayHello()
+{
+    SayHello();
+    return "hi";
+}
 
-// SayHello();
+SayHello();
 
-// while(1>0) 
-// {
-//     Console.WriteLine("Hi");
-// }
+while(1>0) 
+{
+    Console.WriteLine("Hi");
+}
 
-// int Factorial(int value)
-// {
-//     // базовый случай - случай выхода из рекурсии
+int Factorial(int value)
+{
+    // базовый случай - случай выхода из рекурсии
 
-//     // рекурсивный случай (вызов функции внутри функции)
+    // рекурсивный случай (вызов функции внутри функции)
 
-//     //int result = 0;
-//     // for (int i = 0; i < value; i++)
-//     // {
-//     if (value == 1) return 1;
-//     return value * Factorial(value-1);
-// }
-// Console.Write("Введите число: ");
-// int Number = Convert.ToInt32(Console.ReadLine());
-// /// <summary>
-// /// 
-// /// </summary>
-// /// <param name="start">числа от одного</param>
-// /// <param name="end">переменная Numder</param>
-// /// <returns></returns>
-// string PrintNumber(int start, int end)
-// {
-//     if(start == end) return end.ToString(); //  преобразует число в формата "Число"
+    //int result = 0;
+    // for (int i = 0; i < value; i++)
+    // {
+    if (value == 1) return 1;
+    return value * Factorial(value-1);
+}
+Console.Write("Введите число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+/// <summary>
+/// 
+/// </summary>
+/// <param name="start">числа от одного</param>
+/// <param name="end">переменная Numder</param>
+/// <returns></returns>
+string PrintNumber(int start, int end)
+{
+    if(start == end) return end.ToString(); //  преобразует число в формата "Число"
 
-//     return (start + "," + PrintNumber(start+1, end));
-// }
-// Console.WriteLine(PrintNumber(1,Number));
+    return (start + "," + PrintNumber(start+1, end));
+}
+Console.WriteLine(PrintNumber(1,Number));
 
 
 Console.Write("Введите число: ");
@@ -82,3 +82,17 @@ if (Num == 0) return 0; // num % 10 = делит число на 10 если 100
 return(Num % 10 + SumNumber(Num / 10));
 }
 Console.WriteLine(SumNumber(Number));
+
+Console.Write("Введите число А: ");
+int NumberА = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите степень числа: ");
+int degree = Convert.ToInt32(Console.ReadLine());
+
+int GetCalc(int Number, int power)
+{
+    if (power == 0) return 1; // можно так, но нужна доп проверка на ввод 0 (power = 1) return Number
+    return (Number * GetCalc(Number, power-1));
+}
+
+Console.WriteLine(GetCalc(NumberА,degree));
---
+++ homework9/task1/Program.cs
@@ -0,0 +1,27 @@
+﻿/// <summary>
+/// проверка на число, если нет то возвращает сообщение
+/// </summary>
+/// <param name="message">вывод сообщения</param>
+/// <returns></returns>
+
+int GetNumberConsole(string message)
+{
+input1:
+    Console.Write(message);
+    bool check_Number = int.TryParse(Console.ReadLine(), out int Number);
+    if (!check_Number)
+    {
+        Console.WriteLine("Введены не верные данные");
+        goto input1;
+    }
+    return Number;
+}
+
+string PrintNumber(int end, int start)
+{
+    if(end == start) return end.ToString(); //  преобразует число в формата "Число"
+
+    return (end + "," + PrintNumber(end-1, start));
+}
+int Number = GetNumberConsole("Введите число: ");
+Console.WriteLine(PrintNumber(Number,1));
