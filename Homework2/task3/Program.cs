10cf636ca6038ff42f8cebdca9cc846fa8064b49
Sergey Bushelenkov <bsn1980@yandex.ru>
2023-03-27 18:18:27 +0300

    Создали папку

--- Homework2/task3/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿Console.Write("Введите день недели от 1 до 7: "); // вводим первое число
-int Day = Convert.ToInt32(Console.ReadLine()); // вносим в переменную
-
-if (Day == 6 || Day == 7)
-{
-    Console.WriteLine("Это выходной день");
-    return;
-}
-Console.WriteLine("Это рабочий день");
